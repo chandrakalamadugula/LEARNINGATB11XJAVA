bf6b4dfa8d1b42bb9d5befd3635e7903939cb607
cmadugula <97531069+cmadugula@users.noreply.github.com>
2025-03-19 20:40:09 +0530

    Lab001 first program commit

---
+++ src/Ex_01_JAVA_BASIC/Lab004_Comments.java
@@ -0,0 +1,11 @@
+package Ex_01_JAVA_BASIC;
+
+public class Lab004_Comments {
+    public static   void main(String[] args){
+        //This program is to demonstrate comments. This is a single line comment.
+       /* This is s a multi line comment
+                Comments are ignored
+                Shortcut ctrl+shift+/ can be used to comment multiple lines*/
+        System.out.println("Program for comments");
+    }
+}
