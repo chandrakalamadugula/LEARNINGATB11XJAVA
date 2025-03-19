1181ceecb7de26317d914dbd78d44dce6fba3c51
cmadugula <97531069+cmadugula@users.noreply.github.com>
2025-03-19 21:06:22 +0530

    Program Committed.

---
+++ src/Ex_01_JAVA_BASIC/Lab013_Println_vs_Print.java
@@ -0,0 +1,11 @@
+package Ex_01_JAVA_BASIC;
+
+public class Lab013_Println_vs_Print {
+public static void main(String[] args){
+    System.out.println("Print new line demo");
+    System.out.println("Print newline demo");
+
+    System.out.print("Print demo");
+    System.out.print("Print demo");
+}
+}
