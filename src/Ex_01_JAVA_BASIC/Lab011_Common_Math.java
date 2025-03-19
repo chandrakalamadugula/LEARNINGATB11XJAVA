09bdf0b469e27e8990feb5a74506088fd8851748
cmadugula <97531069+cmadugula@users.noreply.github.com>
2025-03-19 20:55:35 +0530

    Program Committed.

---
+++ src/Ex_01_JAVA_BASIC/Lab011_Common_Math.java
@@ -0,0 +1,11 @@
+package Ex_01_JAVA_BASIC;
+
+public class Lab011_Common_Math {
+    public static void main(String[] args){
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(2-2);
+        System.out.println(3/2);  //println prints integer part
+    }
+}
