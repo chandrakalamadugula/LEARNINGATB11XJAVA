16ec26857d23650246ccfa8d641f7a92d0f3510e
cmadugula <97531069+cmadugula@users.noreply.github.com>
2025-03-19 20:51:56 +0530

    Program Committed.

---
+++ src/Ex_01_JAVA_BASIC/Lab010_Keywords_Identifier.java
@@ -0,0 +1,8 @@
+package Ex_01_JAVA_BASIC;
+
+public class Lab010_Keywords_Identifier {
+
+    public static void main(String[] args){
+        System.out.println("Program for Keywords, Identifiers");
+    }
+}
