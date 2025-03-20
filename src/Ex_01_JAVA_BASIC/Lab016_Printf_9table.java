f90c3d9a6b9c16d3efc8a233ab9768cae2dda45e
cmadugula <97531069+cmadugula@users.noreply.github.com>
2025-03-20 15:48:44 +0530

    Program Committed.

---
+++ src/Ex_01_JAVA_BASIC/Lab016_Printf_9table.java
@@ -0,0 +1,17 @@
+package Ex_01_JAVA_BASIC;
+
+public class Lab016_Printf_9table {
+    static public void main(String[] args){
+        int a=9;
+        System.out.printf("9*1=%d\n",a*1);
+        System.out.printf("9*1=%d\n",a*2);
+        System.out.printf("9*1=%d\n",a*3);
+        System.out.printf("9*1=%d\n",a*4);
+        System.out.printf("9*1=%d\n",a*5);
+        System.out.printf("9*1=%d\n",a*6);
+        System.out.printf("9*1=%d\n",a*7);
+        System.out.printf("9*1=%d\n",a*8);
+        System.out.printf("9*1=%d\n",a*9);
+        System.out.printf("9*1=%d\n",a*10);
+    }
+}
