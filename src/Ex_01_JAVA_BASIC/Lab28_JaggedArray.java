2d598fb1cd921d50b6e130b9eee5dc0efa404189
cmadugula <97531069+cmadugula@users.noreply.github.com>
2025-03-30 17:14:17 +0530

    Program Committed.

--- src/Ex_01_JAVA_BASIC/Lab28_JaggedArray.java
+++ src/Ex_01_JAVA_BASIC/Lab28_JaggedArray.java
@@ -8,7 +8,7 @@ public class Lab28_JaggedArray {
         int[][] jag_Array = { {1,2,3},{4,5,6,7},{7}};
         for (int i = 0; i < jag_Array.length; i++) {      //jag_array.length gives the length of the array i.e how many rows
             for (int j = 0; j < jag_Array[i].length; j++) //jag_array[i].length gives the length of the i th row  i.e how many columns it has
-                System.out.print("" + jag_Array[i][j] + " ");
+                System.out.print(" " + jag_Array[i][j] + " ");
             System.out.println("\n");
 
         }
