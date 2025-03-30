12f119a6c12985f3a7d0edfcd5c703d4513cbd8a
cmadugula <97531069+cmadugula@users.noreply.github.com>
2025-03-30 17:14:57 +0530

    Program Committed.

---
+++ src/Ex_01_JAVA_BASIC/lab29_2D_p0.java
@@ -0,0 +1,20 @@
+package Ex_01_JAVA_BASIC;
+
+public class lab29_2D_p0 {
+    public static void main(String[] args){
+
+        int[][] matrix = new int[3][3];
+        matrix[0][0] = 1;
+        matrix[0][1]=2;
+        matrix[0][2]=3;
+
+        matrix[1][0] = 4;
+        matrix[1][1]=5;
+        matrix[1][2]=6;
+
+        matrix[2][0] = 7;
+        matrix[2][1]=8;
+        matrix[2][2]=9;
+
+    }
+}
